b99cbdb0abba9f305cc698621ac05cf3015c930f
Dan Printzell <wildn00b@softwepz.co.cc>
2012-06-10 21:23:28 +0200

    Added bukkit base Signed-off-by: Dan Printzell <wildn00b@softwepz.co.cc>

---
+++ src/me/wildn00b/prekick/PreKick.java
@@ -0,0 +1,17 @@
+package me.wildn00b.prekick;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PreKick extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
