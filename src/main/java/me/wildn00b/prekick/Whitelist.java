37de3d67c2b52245ff40c7cb3940a21de82e0691
Dan Printzell <wildn00b@softwepz.co.cc>
2012-08-26 03:53:31 +0200

    Last minute change: * Changed to the player check is case insensitive. Signed-off-by: Dan Printzell <wildn00b@softwepz.co.cc>

--- src/main/java/me/wildn00b/prekick/Whitelist.java
+++ src/main/java/me/wildn00b/prekick/Whitelist.java
@@ -1,7 +1,9 @@
 package me.wildn00b.prekick;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 
 import org.bukkit.ChatColor;
@@ -18,6 +20,34 @@ public class Whitelist {
 		return text.replaceAll("&", "\u00A7");
 	}
 
+	private boolean containsIgnoreCase(List<String> list, String text) {
+		Iterator<String> it = list.iterator();
+		while (it.hasNext()) {
+			if (it.next().equalsIgnoreCase(text))
+				return true;
+		}
+		return false;
+	}
+	
+	private boolean containsIgnoreCase(Set<String> list, String text) {
+		Iterator<String> it = list.iterator();
+		while (it.hasNext()) {
+			if (it.next().equalsIgnoreCase(text))
+				return true;
+		}
+		return false;
+	}
+	
+	private String getCase(List<String> list, String text) {
+		Iterator<String> it = list.iterator();
+		while (it.hasNext()) {
+			String tmp = it.next();
+			if (tmp.equalsIgnoreCase(text))
+				return tmp;
+		}
+		return "";
+	}
+
 	public int IsPlayerOnWhitelist(String player, String IP) { // Returns 0 = not on the whitelist, 1 = on the
 																// whitelist, 2 = wrong ip, 3 = on the blacklist
 		if (prekick.config.GetBoolean("Blacklist.Enabled")) {
@@ -25,7 +55,7 @@ public class Whitelist {
 			if (prekick.config.GetBoolean("Blacklist.Groups.Enabled")) {
 				for (String group : prekick.config.GetKeys("Blacklist.Groups")) {
 					if (group != "Enabled") {
-						if (prekick.config.GetStringList("Blacklist.Groups." + group + ".Players").contains(player))
+						if (containsIgnoreCase(prekick.config.GetStringList("Blacklist.Groups." + group + ".Players"), player))
 							return 4;
 					}
 				}
@@ -33,21 +63,21 @@ public class Whitelist {
 			// Blacklist group stop
 
 			// Blacklist player start
-			if (prekick.config.GetKeys("Blacklist.Players").contains(player))
+			if (containsIgnoreCase(prekick.config.GetKeys("Blacklist.Players"), player))
 				return 3;
 			// Blacklist player stop
 		}
 
 		// Whitelist start
 		if (prekick.config.GetBoolean("Whitelist.Enabled"))
-			if (!prekick.config.GetStringList("Whitelist.Players").contains(player))
+			if (!containsIgnoreCase(prekick.config.GetStringList("Whitelist.Players"), player))
 				return 0;
 		// Whitelist stop
 
 		// IP whitelist start
 		if (prekick.config.GetBoolean("IP.Enabled"))
 			if (prekick.config.GetKeys("IP.Players").contains(player) && !IP.equals("NULL"))
-				if (!prekick.config.GetStringList("IP.Players." + player).contains(IP))
+				if (!containsIgnoreCase(prekick.config.GetStringList("IP.Players." + player), IP))
 					return 2;
 		// IP whitelist stop
 
@@ -64,7 +94,7 @@ public class Whitelist {
 		else if (reason == 4) {
 			for (String group : prekick.config.GetKeys("Blacklist.Groups")) {
 				if (group != "Enabled") {
-					if (prekick.config.GetStringList("Blacklist.Groups." + group + ".Players").contains(player))
+					if (containsIgnoreCase(prekick.config.GetStringList("Blacklist.Groups." + group + ".Players"), player))
 						return prekick.config.GetString("Blacklist.Groups." + group + ".KickMessage");
 				}
 			}
@@ -77,7 +107,7 @@ public class Whitelist {
 	}
 
 	public String AddPlayerToWhitelist(String player) {
-		if (prekick.config.GetStringList("Whitelist.Players").contains(player))
+		if (containsIgnoreCase(prekick.config.GetStringList("Whitelist.Players"), player))
 			return "[" + ChatColor.RED + "PreKick" + ChatColor.RESET + "] " + prekick.language.GetText("Whitelist.Whitelist.AlreadyAdded").replaceAll("%PLAYER%", player);
 
 		List<String> list = prekick.config.GetStringList("Whitelist.Players");
@@ -89,11 +119,12 @@ public class Whitelist {
 	}
 
 	public String RemovePlayerFromWhitelist(String player) {
-		if (!prekick.config.GetStringList("Whitelist.Players").contains(player))
+		if (!containsIgnoreCase(prekick.config.GetStringList("Whitelist.Players"), player))
 			return "[" + ChatColor.RED + "PreKick" + ChatColor.RESET + "] " + prekick.language.GetText("Whitelist.Whitelist.NotOnList").replaceAll("%PLAYER%", player);
 
 		List<String> list = prekick.config.GetStringList("Whitelist.Players");
-		list.remove(player);
+		
+		list.remove(getCase(list, player));
 		prekick.config.Set("Whitelist.Players", list);
 		prekick.config.Save();
 
@@ -101,7 +132,7 @@ public class Whitelist {
 	}
 
 	public String AddPlayerToIPWhitelist(String player, String IP) {
-		if (prekick.config.GetKeys("IP.Players").contains(player)) {
+		if (containsIgnoreCase(prekick.config.GetKeys("IP.Players"), player)) {
 			List<String> list = prekick.config.GetStringList("IP.Players." + player);
 			list.add(IP);
 			prekick.config.Set("IP.Players." + player, list);
@@ -116,7 +147,7 @@ public class Whitelist {
 	}
 
 	public String RemovePlayerFromIPWhitelist(String player, String IP) {
-		if (prekick.config.GetKeys("IP.Players").contains(player)) {
+		if (containsIgnoreCase(prekick.config.GetKeys("IP.Players"), player)) {
 			if (IP == "NULL") {
 				List<String> list = prekick.config.GetStringList("IP.Players." + player);
 				list.clear();
@@ -126,7 +157,7 @@ public class Whitelist {
 				return "[" + ChatColor.RED + "PreKick" + ChatColor.RESET + "] " + prekick.language.GetText("Whitelist.IP.RemovedIP").replaceAll("%PLAYER%", player);
 			} else {
 				List<String> list = prekick.config.GetStringList("IP.Players." + player);
-				list.remove(IP);
+				list.remove(getCase(list, IP));
 				if (list.size() == 0)
 					prekick.config.getConfig().getConfigurationSection("IP.Players").set(player, null);
 				else
@@ -140,7 +171,7 @@ public class Whitelist {
 	}
 
 	public String AddPlayerToBlacklist(String player, String group) {
-		if (prekick.config.GetStringList("Blacklist.Groups." + group + ".Players").contains(player))
+		if (containsIgnoreCase(prekick.config.GetStringList("Blacklist.Groups." + group + ".Players"), player))
 			return "[" + ChatColor.RED + "PreKick" + ChatColor.RESET + "] " + prekick.language.GetText("Whitelist.Blacklist.AlreadyAdded").replaceAll("%PLAYER%", player);
 
 		List<String> list = prekick.config.GetStringList("Blacklist.Groups." + group + ".Players");
@@ -152,11 +183,11 @@ public class Whitelist {
 	}
 
 	public String RemovePlayerFromBlacklist(String player, String group) {
-		if (!prekick.config.GetStringList("Blacklist.Groups.Players").contains(player))
+		if (!containsIgnoreCase(prekick.config.GetStringList("Blacklist.Groups.Players"), player))
 			return "[" + ChatColor.RED + "PreKick" + ChatColor.RESET + "] " + prekick.language.GetText("Whitelist.Blacklist.NotOnList").replaceAll("%PLAYER%", player);
 
 		List<String> list = prekick.config.GetStringList("Blacklist.Groups." + group + ".Players");
-		list.remove(player);
+		list.remove(getCase(list, player));
 		prekick.config.Set("Blacklist.Groups." + group + ".Players", list);
 		prekick.config.Save();
 
